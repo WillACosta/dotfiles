0e4d3a17567ebe93df9440dd79032785af26140d
willACosta <williamamaral521@gmail.com>
2025-08-02 19:44:21 -0300

    refactor: added new options to the tmux config file

--- tmux/start_dev.sh
+++ tmux/start_dev.sh
@@ -8,14 +8,19 @@ if ! tmux has-session -t "$SESSION" 2>/dev/null; then
   # Create detached session with the first window
   tmux new-session -d -s "$SESSION" -n "workspace"
 
+  # Runs every new panel is created
+  set-hook -g after-split-window 'run-shell "tmux send-keys -t #{pane_id} \"source ~/.zshrc && cd ~/workspace\" Enter"'
+
   # Split the first window vertically (pane 1)
   tmux split-window -v -t "$SESSION:workspace"
 
   # Send commands to pane 1
   tmux send-keys -t "$SESSION:workspace.1" "cd $WORKDIR" C-m
+  tmux send-keys -t "$SESSION:workspace.1" "conda deactivate" C-m
 
   # Send commands to pane 2
   tmux send-keys -t "$SESSION:workspace.2" "cd $WORKDIR" C-m
+  tmux send-keys -t "$SESSION:workspace.2" "conda deactivate" C-m
 
   # Create second window for llm
   tmux new-window -t "$SESSION" -n "llm"
